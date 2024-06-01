ddc567e364b8769fb6450620da0ca79bb8d1230a
Michel <146105710+I-Michel@users.noreply.github.com>
2024-06-01 18:24:28 +0200

    Fixing files

---
+++ SPRINT4/Tasca2/S04T02N01Michel/src/main/java/cat/itacademy/barcelonactiva/Michel/S04/T02/N01/S04T02N01MichelApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Michel.S04.T02.N01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01MichelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01MichelApplication.class, args);
+	}
+
+}
---
+++ SPRINT4/Tasca2/S04T02N01Michel/src/test/java/cat/itacademy/barcelonactiva/Michel/S04/T02/N01/S04T02N01MichelApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Michel.S04.T02.N01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01MichelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
